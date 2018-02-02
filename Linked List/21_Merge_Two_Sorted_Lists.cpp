f0bfe4447a98d038a631b4349eec0132ed357852
hellozgm <hellozgm@163.com>
2018-02-02 17:12:29 +0800

    method 2

--- Linked List/21_Merge_Two_Sorted_Lists.cpp
+++ Linked List/21_Merge_Two_Sorted_Lists.cpp
@@ -9,7 +9,7 @@
 
 /**
  *这道题没必要开辟新的内存空间，直接复用l1,l2即可，第二种解法是用递归解决。
-
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
@@ -32,5 +32,22 @@ public:
         else
             rear->next = l2;
         return dummyHead->next;
+    }
+};
+
+/**方法2**/
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
     }
 };
